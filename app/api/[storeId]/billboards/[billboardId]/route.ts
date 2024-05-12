fe2dd16eda7d3043179f4923a57bbd282ee25fcf
Mofijur Rahman <133999152+Noyon141@users.noreply.github.com>
2024-05-13 00:54:13 +0600

    Removed the protection of Public/GET route so that unauthenticated user can see billboard

--- app/api/[storeId]/billboards/[billboardId]/route.ts
+++ app/api/[storeId]/billboards/[billboardId]/route.ts
@@ -9,12 +9,6 @@ export async function GET(
   { params }: { params: { billboardId: string } }
 ) {
   try {
-    const { userId } = auth();
-
-    if (!userId) {
-      return new NextResponse("UNAUTHENTICATED", { status: 401 });
-    }
-
     if (!params.billboardId) {
       return new NextResponse("BILLBOARD ID IS REQUIRED", { status: 400 });
     }

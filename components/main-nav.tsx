080faf1070940a379b56dc6401babadc8c153bcc
Mofijur Rahman <133999152+Noyon141@users.noreply.github.com>
2024-05-15 01:13:13 +0600

    Created categories model schema and added categories route in main-nav

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -34,14 +34,19 @@ export const MainNav = ({
     },
 
     {
-      href: `/${params.storeId}/settings`,
-      label: "Settings",
-      active: pathname === `/${params.storeId}/settings`,
-    },
-    {
       href: `/${params.storeId}/billboards`,
       label: "Billboards",
       active: pathname === `/${params.storeId}/billboards`,
+    },
+    {
+      href: `/${params.storeId}/categories`,
+      label: "Categories",
+      active: pathname === `/${params.storeId}/categories`,
+    },
+    {
+      href: `/${params.storeId}/settings`,
+      label: "Settings",
+      active: pathname === `/${params.storeId}/settings`,
     },
   ];
 
